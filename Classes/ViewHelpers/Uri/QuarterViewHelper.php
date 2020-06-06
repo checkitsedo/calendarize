05a1e6630ecf3d63a3b924c22a95c8be32511282
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-06-06 14:10:49 +0200

    Update QuarterViewHelper.php
    Change namespace and vendor name

--- Classes/ViewHelpers/Uri/QuarterViewHelper.php
+++ Classes/ViewHelpers/Uri/QuarterViewHelper.php
@@ -5,12 +5,12 @@
  */
 declare(strict_types=1);
 
-namespace HDNET\Calendarize\ViewHelpers\Uri;
+namespace Checkitsedo\Calendarize\ViewHelpers\Uri;
 
 /**
  * Uri to the quarter.
  */
-class QuarterViewHelper extends \HDNET\Calendarize\ViewHelpers\Link\QuarterViewHelper
+class QuarterViewHelper extends \Checkitsedo\Calendarize\ViewHelpers\Link\QuarterViewHelper
 {
     /**
      * Render the uri to the given quarter.

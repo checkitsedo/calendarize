651407a6d6ebc5dcd7b16104dbbc91d32ca88951
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-06-06 14:16:47 +0200

    Update TitleTagViewHelper.php
    Change namespace and vendor name

--- Classes/ViewHelpers/TitleTagViewHelper.php
+++ Classes/ViewHelpers/TitleTagViewHelper.php
@@ -5,7 +5,7 @@
  */
 declare(strict_types=1);
 
-namespace HDNET\Calendarize\ViewHelpers;
+namespace Checkitsedo\Calendarize\ViewHelpers;
 
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;

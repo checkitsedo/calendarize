747036ecb05343ecef917ff56bb9ce221e42c180
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-06-06 14:16:14 +0200

    Update LanguageViewHelper.php
    Change namespace and vendor name

--- Classes/ViewHelpers/LanguageViewHelper.php
+++ Classes/ViewHelpers/LanguageViewHelper.php
@@ -5,7 +5,7 @@
  */
 declare(strict_types=1);
 
-namespace HDNET\Calendarize\ViewHelpers;
+namespace Checkitsedo\Calendarize\ViewHelpers;
 
 use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 

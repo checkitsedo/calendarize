b2850d00e50b257cd82c37aa2e8d33548b95c0ba
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-06-06 12:01:20 +0200

    Update EventRepository.php
    Change namespace and vendor name

--- Classes/Domain/Repository/EventRepository.php
+++ Classes/Domain/Repository/EventRepository.php
@@ -5,10 +5,10 @@
  */
 declare(strict_types=1);
 
-namespace HDNET\Calendarize\Domain\Repository;
+namespace Checkitsedo\Calendarize\Domain\Repository;
 
-use HDNET\Calendarize\Domain\Model\Event;
-use HDNET\Calendarize\Domain\Model\Index;
+use Checkitsedo\Calendarize\Domain\Model\Event;
+use Checkitsedo\Calendarize\Domain\Model\Index;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Extbase\Persistence\Generic\Query;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
